f918dd5f6dd63ab759e95f01368ff65e516a1c67
ZisnerNeo <zisnerneo@gmail.com>
2022-02-25 08:52:11 -0600

    chale moment

---
+++ PointofSale.java
@@ -0,0 +1,23 @@
+package uaslp.objetos.parcial1.examen.pos;
+
+import java.util.ArrayList;
+
+public class PointOfSale {
+    private ArrayList<CartItem> item;
+
+    public void addToCart(int quantity, Product product){
+
+    }
+
+    public float getTotal(){
+        //return;
+    }
+
+    public ArrayList<CartItem> getItem() {
+        return item;
+    }
+
+    public int getCountByName(String name){
+        //return;
+    }
+}
